2d4ddd806ced50c93e568e38665cd257c4c1894c
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-07-28 14:30:40 -0400

    Initialised app, testing out components

---
+++ src/pages/Inbox/Inbox.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import listData from "../../data";
+import Column from "../../components/Column/Column";
+
+class Inbox extends Component {
+    state = listData;
+    render() {
+        // This stores all our columns and maps them
+        return this.state.columnOrder.map((columnId) => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+             return <Column key={column.id} column={column} tasks={tasks}/>
+        });
+    }
+}
+
+export default Inbox;
