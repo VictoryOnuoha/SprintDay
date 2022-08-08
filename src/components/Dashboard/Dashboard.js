1fb97d788df2f318aeae4dba7482d0b56687b27a
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-08-08 13:22:27 -0400

    Created jsx and styling for dashboard on mobile view

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,24 @@
-
+import './Dashboard.scss';
 
 function Dashboard () {
     return (
-        <main>
-            This is Dashboard
+        <main className='dashboard'>
+            <article className="dashboard__title-box" > 
+                <h3 className='dashboard__title' >DASHBOARD</h3>
+            </article>
+            <article className='dashboard__message-box'>
+                <p className='dashboard__message'>You have 4 tasks to do.</p>
+                <p className='dashboard__message'>You have 5 days left till Sprint ends</p>
+            </article>
+            
+            <article className='dashboard__active' >
+                <h3 className='dashboard__active-title'>DUE SOON</h3>
+            </article>
+            <section className='dashboard__tasks' >
+                <p className='dashboard__task'> P1 Meeting with investors</p>
+                <p className='dashboard__task'> P2 Brand Photoshoot</p>
+                <p className='dashboard__task' > P3 BET Brainstorm session</p>
+            </section>
         </main>
     )
 }
