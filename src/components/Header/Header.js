41c89d55101e1201bdc66af84ff73ee3e7cfb5c0
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-08-08 14:06:10 -0400

    Created styling for header and dashboard for tablet and desktop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,32 +21,34 @@ class Header extends Component {
                 <h1 className='header__logo-text' >SPRINTDAY</h1>
             </section>
 
-            <section className='header__user' >
-                <article className='header__image' >
-                    <img className='header__avatar' src={avatar} alt='avatar of user' />
-                </article>
+            <section className='header__navigation'>
+                <section className='header__user' >
+                    <article className='header__image' >
+                        <img className='header__avatar' src={avatar} alt='avatar of user' />
+                    </article>
 
-                <article className='header__title-box' >
-                    <h2 className='header__title' >Hello Stella!</h2>
-                </article>
-                
-            </section>
-            {/* Will be displayed in tablet and desktop */}
-            <section className='header__nav' >
-                <ul className='header__list'  >
-                    <li className='header__list-item' >DASHBOARD</li>
-                    <li className='header__list-item' >PROJECTS</li>
-                    <li className='header__list-item' >TASKS</li>
-                </ul>
-            </section>
+                    <article className='header__title-box' >
+                        <h2 className='header__title' >Hello Stella!</h2>
+                    </article>
+                    
+                </section>
+                {/* Will be displayed in tablet and desktop */}
+                <section className='header__nav' >
+                    <ul className='header__list'  >
+                        <li className='header__list-item' >DASHBOARD</li>
+                        <li className='header__list-item' >PROJECTS</li>
+                        <li className='header__list-item' >TASKS</li>
+                    </ul>
+                </section>
 
-            {/* Will be displayed in mobile: Hamburger menu */}
-            <section className='header__hamburger' onClick={handleClick}  >
-                <div className='header__hamburger1'></div>
-                <div className='header__hamburger2'></div>
-                <div className='header__hamburger3'></div>
-            </section>
+                {/* Will be displayed in mobile: Hamburger menu */}
+                <section className='header__hamburger' onClick={handleClick}  >
+                    <div className='header__hamburger1'></div>
+                    <div className='header__hamburger2'></div>
+                    <div className='header__hamburger3'></div>
+                </section>
 
+            </section>
         </header>
     )
     }
